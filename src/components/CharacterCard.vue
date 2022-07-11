e14f4fa6aadb8ed9e34bd2f0bbe4f6274717b6be
FirsovDmitriy <dfirsov@nopreset.ru>
2022-07-11 19:46:54 +0400

    added pagination

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="character">
     <div class="character__img-wrapper">
-      img
+      <img :src="character.image" alt="" class="character__img">
     </div>
     <div class="character__content-wrapper">
       <div class="character__row">
         <a href="#" class="character__title"> {{ character.name }} </a>
+        <div class="character__status">
+          <span class="character__status-icon"></span>
+          <p class="chracter__text"> {{ character.status }} - {{ character.species }} </p>
+        </div>
       </div>
-      <div class="character__row"></div>
-      <div class="character__row"></div>
+      <div class="character__row">
+        <p class="character__text character__text--gray">Last known location:</p>
+        <a href="#" class="character__link" _target="blank">
+          {{ character.location.name }}
+        </a>
+      </div>
+      <div class="character__row">
+        <p class="character__text character__text--gray">Last known location:</p>
+        <a href="#" class="character__link" _target="blank">
+          {{  }}
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -29,5 +43,11 @@ export default {
 <style scoped>
 .character {
   display: flex;
+}
+.character__content-wrapper {
+  padding: 10px;
+}
+.character__row:not(:last-child) {
+  margin-bottom: 16px;
 }
 </style>
--- src/views/RickAndMortyList.vue
+++ src/views/RickAndMortyList.vue
@@ -1,28 +1,41 @@
 <template>
-  <div>list</div>
   <div class="character-showcase">
     <character-card
       v-for="character in characters" :key="character.id"
       :character="character"
     />
   </div>
+  <div class="pagination-block">
+    <div class="pagination-block__button-group">
+      <button @click="page--" v-show="page > 1" class="pagination-block__button-prev">
+        <chevron-icon class="pagination-block__icon" />
+      </button>
+      <button v-show="page > 1" class="pagination-block__button"> {{ page - 1 }} </button>
+      <button class="pagination-block__button pagination-block__button--active"> {{ page }} </button>
+      <button class="pagination-block__button"> {{ page + 1 }} </button>
+      <button @click="page++" v-show="page < pages" class="pagination-block__button-next">
+        <chevron-icon class="pagination-block__chevron pagination-block__chevron--right" />
+      </button>
+       <button class="pagination-block__button"> {{ pages }} </button>
+    </div>
+  </div>
 </template>
 
 <script>
 import CharacterCard from '@/components/CharacterCard'
-
-import { mapGetters, mapActions } from 'vuex'
+import ChevronIcon from '@/components/icons/ChevronIcon'
+import { mapState, mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'rick-and-morty-list',
 
   components: {
-    CharacterCard
+    CharacterCard, ChevronIcon
   },
 
   data () {
     return {
-      currentPage: 1
+      page: 1
     }
   },
 
@@ -33,17 +46,34 @@ export default {
   },
 
   computed: {
+    ...mapState([
+      'pages'
+    ]),
+
     ...mapGetters([
       'getCharacterByPage'
     ]),
 
     characters () {
-      return this.getCharacterByPage(this.currentPage)
+      return this.getCharacterByPage(this.page)
+    },
+
+    pagination () {
+      return 4
+    }
+  },
+
+  watch: {
+    page: {
+      handler (newValue) {
+        this.getCharacters(newValue)
+      },
+      immediate: true
     }
   },
 
   mounted () {
-    this.getCharacters(this.currentPage)
+    console.log('page', this.page, '|', 'pages', this.pages)
   }
 }
 </script>
@@ -54,5 +84,42 @@ export default {
   grid-template-columns: repeat(auto-fit, minmax(calc(33.333% - 60px), 1fr));
   row-gap: 30px;
   column-gap: 30px;
+}
+
+.pagination-block__button-group {
+  display: flex;
+  justify-content: center;
+}
+
+.pagination-block__button-prev, .pagination-block__button-next, .pagination-block__button {
+  font-size: 14px;
+  min-width: 64px;
+  height: 36px;
+  padding: 0 16px;
+  color: #fff;
+  background-color: #1867c0;
+  border: none;
+}
+
+.pagination-block__button {
+  background-color: transparent;
+  color: #2c3e50;
+}
+
+.pagination-block__button--active {
+  background-color: navy;
+  color: #fff;
+}
+
+.pagination-block__button-prev {
+  margin-right: 10px;
+}
+
+.pagination-block__button-next {
+  margin-left: 10px;
+}
+
+.pagination-block__chevron--right {
+  transform: rotate(180deg);
 }
 </style>
